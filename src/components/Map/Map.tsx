2b856dd4abe49c6a09805475421842d56e8aa00d
Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-03-05 16:40:22 +0700

    bugfix/map in tour detail (#59)

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,21 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { LatLng, LatLngExpression, divIcon, point } from 'leaflet'
+import { LatLng, LatLngExpression, LatLngTuple, divIcon, point } from 'leaflet'
 import { OpenStreetMapProvider } from 'leaflet-geosearch'
 import 'leaflet/dist/leaflet.css'
-import { useEffect, useState } from 'react'
-import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { MapContainer, Marker, TileLayer, useMap, useMapEvents } from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-cluster'
 import { Location } from 'src/types/tour.type'
 import './react-leaflet.css'
 import MapSearchControl from '../MapSearchControl/MapSearchControl'
 
-interface Props {
+interface MapProps {
   onMarkersUpdate: (markers: LatLngExpression[]) => void
   isSelect?: boolean
   locations?: Location[]
 }
 
-const Map: React.FC<Props> = ({ onMarkersUpdate, isSelect, locations }: Props) => {
+const Map: React.FC<MapProps> = ({ onMarkersUpdate, isSelect, locations }: MapProps) => {
+  const [centerLocation, setCenterLocation] = useState<LatLngTuple>([16.047079, 108.20623])
+  const [markers, setMarkers] = useState<LatLngExpression[]>([])
+  const prov = new OpenStreetMapProvider()
+
   const convertLocationToLatLngExpression = (location: Location): LatLngExpression => {
     return {
       lat: location.latitude,
@@ -23,22 +27,30 @@ const Map: React.FC<Props> = ({ onMarkersUpdate, isSelect, locations }: Props) =
     }
   }
 
-  const convertLocationsToLatLngExpressions = (locations: Location[]): LatLngExpression[] => {
+  const convertLocationsToLatLngExpressions = useCallback((locations: Location[]): LatLngExpression[] => {
     return locations.map(convertLocationToLatLngExpression)
-  }
+  }, [])
 
-  const latLngExpressions: LatLngExpression[] = convertLocationsToLatLngExpressions(locations || [])
-
-  const [markers, setMarkers] = useState<LatLngExpression[]>([])
-  const prov = new OpenStreetMapProvider()
+  const formattedMarkers: LatLngExpression[] = useMemo(
+    () => convertLocationsToLatLngExpressions(locations || []),
+    [convertLocationsToLatLngExpressions, locations]
+  )
 
   useEffect(() => {
     onMarkersUpdate(markers)
-  }, [markers, onMarkersUpdate])
+    setMarkers(formattedMarkers)
 
-  useEffect(() => {
-    setMarkers(latLngExpressions)
-  }, [locations])
+    if (markers && markers.length > 0) {
+      setCenterLocation([(markers[0] as LatLng).lat, (markers[0] as LatLng).lng])
+    }
+  }, [markers, onMarkersUpdate, formattedMarkers, locations])
+
+  const ChangeMapView = ({ coords }: { coords: LatLngTuple }) => {
+    const map = useMap()
+    map.setView(coords, map.getZoom())
+
+    return null
+  }
 
   const AddMarkerOnClick = () => {
     useMapEvents({
@@ -70,7 +82,7 @@ const Map: React.FC<Props> = ({ onMarkersUpdate, isSelect, locations }: Props) =
     })
 
   return (
-    <MapContainer center={[16.047079, 108.20623]} zoom={13}>
+    <MapContainer center={centerLocation} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -100,6 +112,7 @@ const Map: React.FC<Props> = ({ onMarkersUpdate, isSelect, locations }: Props) =
           ></Marker>
         ))}
       </MarkerClusterGroup>
+      <ChangeMapView coords={centerLocation} />
     </MapContainer>
   )
 }
