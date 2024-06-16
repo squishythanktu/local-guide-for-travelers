df0742dbf11480930f5434d121f6be9cc83a500a
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-06-16 10:58:51 +0700

    update be url

--- src/constants/config.constant.ts
+++ src/constants/config.constant.ts
@@ -1,5 +1,5 @@
 const config = {
-  baseUrl: 'https://local-guide-be.azurewebsites.net',
+  baseUrl: 'https://local-guide-be-bacrh5gmd5h2c2bp.southeastasia-01.azurewebsites.net/',
   frontEndUrl: 'https://local-guide-for-travelers.vercel.app/'
 }
 
